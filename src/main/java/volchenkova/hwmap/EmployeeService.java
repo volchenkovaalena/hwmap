0f2d381a8de21951e08e05853dff49da0152080e
alena <misamistake@>
2024-04-01 23:59:53 +0300

    initial commit

--- src/main/java/volchenkova/hwmap/EmployeeService.java
+++ src/main/java/volchenkova/hwmap/EmployeeService.java
@@ -4,5 +4,4 @@ public interface EmployeeService {
     Employee add (String name, String surname);
     void remove (String name, String surname);
     Employee find (String name, String surname);
-
 }
